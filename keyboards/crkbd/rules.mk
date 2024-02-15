f0869884b42b3b5734e64a317972f6be291e2f5d
Andras Marczell <30615058+iteroji@users.noreply.github.com>
2024-02-15 10:08:17 +0100

    updated layout

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -1,8 +1,6 @@
 # Build Options
 #   change yes to no to disable
 #
-
 DEFAULT_FOLDER = crkbd/rev1
-
-RGBLIGHT_SUPPORTED = yes
-RGB_MATRIX_SUPPORTED = yes
+RGBLIGHT_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
